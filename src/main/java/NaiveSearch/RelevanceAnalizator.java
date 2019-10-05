fcfee306e240747ae8b0b4e4764e3061ce47e7f0
dariamikl <d.miklashevskaya@innopolis,ru>
2019-10-05 17:35:00 +0300

    + query vectorizer

--- src/main/java/NaiveSearch/RelevanceAnalizator.java
+++ src/main/java/NaiveSearch/RelevanceAnalizator.java
@@ -11,7 +11,64 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
+
 public class RelevanceAnalizator {
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+
+            String query = "the best best the query in the world";
+            String [] tokens = query.split(" ");
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            for (int i = 0; i < tokens.length; i++) {
+                if (!map.containsKey(tokens[i]))
+                        map.put(tokens[i], 1);
+                else map.put(tokens[i], map.get(tokens[i])+1);
+                System.out.println(map);
+            }
+
+
+
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                System.out.println(word);
+                context.write(word, one);
+            }
+        }
+
+    }
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "relevance analizator");
+        job.setJarByClass(RelevanceAnalizator.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
+
